915dcc6cae81bfbfbe982fa3fdd8e334cc0cf6bd
Mark-Su-0204 <marksumuhan@qq.com>
2021-09-05 10:40:16 +0800

    以Fragment形式组织实体详情页

--- project/app/src/main/java/com/example/ksandroidplayerdemo/EntityActivity.java
+++ project/app/src/main/java/com/example/ksandroidplayerdemo/EntityActivity.java
@@ -3,6 +3,7 @@ package com.example.ksandroidplayerdemo;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,13 +13,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.ksandroidplayerdemo.Fragment.CourseFragment;
+import com.example.ksandroidplayerdemo.EntityFragment.PropertyFragment;
+import com.example.ksandroidplayerdemo.EntityFragment.RelationshipFragment;
+import com.example.ksandroidplayerdemo.EntityFragment.QuestionFragment;
 import com.example.ksandroidplayerdemo.Fragment.SubjectFragment;
 import com.example.ksandroidplayerdemo.utils.HttpUtils;
 import com.example.ksandroidplayerdemo.utils.MD5Utils;
@@ -37,7 +44,22 @@ import java.util.List;
 import com.example.ksandroidplayerdemo.bean.Item;
 
 
-public class EntityActivity extends AppCompatActivity{
+public class EntityActivity extends FragmentActivity implements View.OnClickListener{
+
+    private TextView tv_main_title;//标题
+    private TextView tv_back;//返回按钮
+    private RelativeLayout title_bar;
+    private RelativeLayout entity_body;
+    private TextView top_bar_text_property;
+    private RelativeLayout top_bar_property_btn;
+    private TextView top_bar_text_relationship;
+    private RelativeLayout top_bar_relationship_btn;
+    private TextView top_bar_text_question;
+    private RelativeLayout top_bar_question_btn;
+    private LinearLayout entity_top_bar;
+
+
+
     private TextView entityName;
     private String entityUri;
     private List<Item> propertyLabel;
@@ -53,52 +75,66 @@ public class EntityActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entity);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        Init();
+        init();
     }
 
-    private void Init(){
+    private void init(){
+        tv_main_title=findViewById(R.id.tv_main_title);
+        title_bar=findViewById(R.id.title_bar);
 
+        entity_body = findViewById(R.id.entity_body);
+        top_bar_text_property = findViewById(R.id.top_bar_text_property);
+        top_bar_property_btn = findViewById(R.id.top_bar_property_btn);
+        top_bar_text_relationship = findViewById(R.id.top_bar_text_relationship);
+        top_bar_relationship_btn = findViewById(R.id.top_bar_relationship_btn);
+        top_bar_text_question = findViewById(R.id.top_bar_text_question);
+        top_bar_question_btn = findViewById(R.id.top_bar_question_btn);
+        entity_top_bar = findViewById(R.id.entity_top_bar);
+
+        top_bar_property_btn.setOnClickListener(this);
+        top_bar_relationship_btn.setOnClickListener(this);
+        top_bar_question_btn.setOnClickListener(this);
+        //tv_main_title.setText("课程");
+        title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
     }
 
-    public class PropertyAdapter extends RecyclerView.Adapter<PropertyAdapter.ViewHolder> {
-
-        private List<Item> labelList;
-        private List<Item> valueList;
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public TextView propertyLabel;
-            public TextView propertyValue;
-            public ViewHolder(View view) {
-                super(view);
-                propertyLabel = (TextView) view.findViewById(R.id.propertyLabel);
-                propertyValue = (TextView) view.findViewById(R.id.propertyValue);
-            }
-        }
-
-        public PropertyAdapter(List<Item> labels, List<Item> values) {
-            labelList = labels;
-            valueList = values;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.entity_property, parent, false);
-            ViewHolder holder = new ViewHolder(view);
-            return holder;
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            Item label = labelList.get(position);
-            holder.propertyLabel.setText(label.getName());
-            Item value = valueList.get(position);
-            holder.propertyValue.setText(value.getName());
-        }
-
-        @Override
-        public int getItemCount() {
-            return labelList.size();
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.top_bar_property_btn:
+                //getSupportFragmentManager().beginTransaction().add(R.id.main_body,new CourseFragment()).commit();
+                setSelectStatus(0);
+                break;
+            case R.id.top_bar_relationship_btn:
+                //getSupportFragmentManager().beginTransaction().add(R.id.main_body,new ExercisesFragment()).commit();
+                setSelectStatus(1);
+                break;
+            case R.id.top_bar_question_btn:
+                //getSupportFragmentManager().beginTransaction().add(R.id.main_body,new MyinfoFragment()).commit();
+                setSelectStatus(2);
+                break;
         }
     }
 
+    private void setSelectStatus(int index) {
+        switch (index){
+            case 0:
+                top_bar_text_property.setTextColor(Color.parseColor("#0097F7"));
+                top_bar_text_relationship.setTextColor(Color.parseColor("#666666"));
+                top_bar_text_question.setTextColor(Color.parseColor("#666666"));
+                getSupportFragmentManager().beginTransaction().replace(R.id.entity_body,new PropertyFragment()).commit();
+                break;
+            case 1:
+                top_bar_text_property.setTextColor(Color.parseColor("#666666"));
+                top_bar_text_relationship.setTextColor(Color.parseColor("#0097F7"));
+                top_bar_text_question.setTextColor(Color.parseColor("#666666"));
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_body,new RelationshipFragment()).commit();
+                break;
+            case 2:
+                top_bar_text_property.setTextColor(Color.parseColor("#666666"));
+                top_bar_text_relationship.setTextColor(Color.parseColor("#666666"));
+                top_bar_text_question.setTextColor(Color.parseColor("#0097F7"));
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_body,new QuestionFragment()).commit();
+                break;
+        }
+    }
 }
